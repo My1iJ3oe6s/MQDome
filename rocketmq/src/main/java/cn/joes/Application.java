29cb80bcb536e5020795b7de31b76cf2f9c0ebec
万桥 <myijoes136@163.com>
2019-04-04 18:03:18 +0800

    123

---
+++ rocketmq/src/main/java/cn/joes/Application.java
@@ -0,0 +1,22 @@
+package cn.joes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Spring Boot 应用启动类
+ *
+ * @MapperScan 接口类扫描包配置
+ *
+ * Created by myijoes on 18/6/14.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+    }
+}
---
+++ rocketmq/src/main/java/cn/joes/simple/SimpleProducer.java
@@ -0,0 +1,36 @@
+package cn.joes.simple;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * Created by myijoes on 2019/4/2.
+ *
+ * @author wanqiao
+ */
+public class SimpleProducer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+                DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("http://47.107.122.233:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                    "TagA" /* Tag */,
+                    ("Hello RocketMQ " +
+                            i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
